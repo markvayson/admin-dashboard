3c7cb05a6517c79131488d9d6b3b6680dfd8eb2f
markvayson <markvayson@gmail.com>
2023-07-20 20:24:02 +0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+let projects = [
+  {
+    name: "Calculator",
+    details: "Html5, Tailwindcss, JavaScript",
+    github: "https://github.com/markvayson/calculator",
+    liveCode: "https://markvayson.github.io/calculator/",
+    img: "img/calculator.png",
+  },
+  {
+    name: "SketchMaster",
+    details: "Html5, Tailwindcss, JavaScript",
+    github: "https://github.com/markvayson/SketchMaster",
+    liveCode: "https://markvayson.github.io/SketchMaster/",
+    img: "img/sketchmaster.png",
+  },
+  {
+    name: "clashofCattitude",
+    details: "Html5, Tailwindcss, JavaScript",
+    github: "https://github.com/markvayson/clashofCattitude",
+    liveCode: "https://markvayson.github.io/clashofCattitude/",
+    img: "img/ClashofCattitude.png",
+  },
+  {
+    name: "signUpForm",
+    details: "Html5, Tailwindcss, JavaScript",
+    github: "https://github.com/markvayson/signUpForm",
+    liveCode: "https://markvayson.github.io/signUpForm/",
+    img: "img/sign.png",
+  },
+];
